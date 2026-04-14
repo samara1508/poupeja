15e9e85e5f88857dc5217021d0ea4bac2b7a147f
samara.castro <sa_gomes@live.com>
2026-04-14 20:11:15 -0300

    Commit inicial

---
+++ poupeja/src/main/java/com/financeiro/poupeja/PoupejaApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.poupeja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoupejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoupejaApplication.class, args);
+	}
+
+}
---
+++ poupeja/src/test/java/com/financeiro/poupeja/PoupejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.poupeja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoupejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
