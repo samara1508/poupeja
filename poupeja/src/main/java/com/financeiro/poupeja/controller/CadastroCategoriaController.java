0a4c5fcf4ed1a4934f31d017c84694958636ade4
MatheusPorsch <155990907+MatheusPorsch@users.noreply.github.com>
2026-06-23 19:31:29 -0300

    Melhoria na Interface de Categoria 7

--- poupeja/src/main/java/com/financeiro/poupeja/controller/CadastroCategoriaController.java
+++ poupeja/src/main/java/com/financeiro/poupeja/controller/CadastroCategoriaController.java
@@ -5,38 +5,49 @@ import com.financeiro.poupeja.PoupejaApplication;
 import com.financeiro.poupeja.entity.Usuario;
 import com.financeiro.poupeja.exception.AcessoNegadoException;
 import com.financeiro.poupeja.util.SpringFXMLLoader;
-import com.financeiro.poupeja.service.AuthService;
 import com.financeiro.poupeja.service.CategoriaService;
+import com.financeiro.poupeja.service.FormaPagamentoService;
+
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+
+import com.financeiro.poupeja.service.AuthService;
+import com.financeiro.poupeja.service.CategoriaService;
 
 import com.financeiro.poupeja.util.MessageUtils;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 
+import javafx.scene.control.CheckBox;
+
+
 @Component
 public class CadastroCategoriaController {
 
+
     private final SpringFXMLLoader fxmlLoader;
-    private final CategoriaService categoriaService;
-    private final AuthService authService;
+    private CategoriaService categoriaService;
+    private AuthService authService;
+
 
     @FXML
-    private TextField txtNome;
+    private TextField txtDescricao;
 
     @FXML
     private TextField txtLimite;
 
-    public CadastroCategoriaController(
-            CategoriaService categoriaService,
-            SpringFXMLLoader fxmlLoader,
-            AuthService authService){
+    @FXML
+    private CheckBox chkAtivo;
+
+    public CadastroCategoriaController(CategoriaService categoriaService, SpringFXMLLoader fxmlLoader, AuthService authService){
+
         this.categoriaService = categoriaService;
         this.fxmlLoader = fxmlLoader;
         this.authService = authService;
+
     }
-    
 
     @FXML
     public void voltarParaLogin() {
@@ -61,18 +72,95 @@ public class CadastroCategoriaController {
     }
 
     @FXML
-    public void criaCategoria() {
-        String descricao = txtNome.getText();
-        double meta = Double.parseDouble(txtLimite.getText());
-        MessageUtils.informacao("Nome: " + descricao + "; Limite: " + meta + ".");
+    public void salvar() {
+
+        String descricao = txtDescricao.getText();
+        if (descricao == null || descricao.isBlank()) {
+            mostrarErro("Erro",
+                    "Digite um nome para a categoria.");
+            return;
+        }
+
+        Double limite;
+        if(txtLimite.getText().isBlank()){
+            mostrarErro("Erro",
+                    "Número não identificado.");
+            return;
+        } else {
+            limite = Double.parseDouble(txtLimite.getText());
+            if(limite < 0){
+                mostrarErro("Erro",
+                    "Números menores que zero não são aceitos.");
+                return;
+            }
+        }
+
+        boolean ativo = chkAtivo.isSelected();
+
 
         try {
+
             Usuario usuarioLogado = authService.getUsuarioLogado();
-            categoriaService.cadastrarCategoria(null, descricao, meta, true, usuarioLogado);
-            MessageUtils.sucesso("Categoria cadastrada com sucesso!");
-            voltarParaLogin();
-        } catch (IllegalArgumentException | AcessoNegadoException e) {
-            MessageUtils.erro(e.getMessage());
+
+            categoriaService.criarCategoria(
+                    null,
+                    descricao,
+                    limite,
+                    ativo,
+                    usuarioLogado
+            );
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+            alert.setTitle("Sucesso");
+            alert.setHeaderText(null);
+            alert.setContentText(
+                    "Categoria cadastrada com sucesso!");
+
+            alert.showAndWait();
+
+            voltar();
+
+        } catch (IllegalArgumentException e) {
+
+            mostrarErro("Erro", e.getMessage());
         }
+    }
+
+
+
+    @FXML
+    public void voltar() {
+
+        navegarPara("/fxml/lista_categoria.fxml",
+                "PoupeJá! - Listagem de Categorias");
+    }
+
+    private void navegarPara(String fxmlPath, String titulo) {
+
+        try {
+
+            Parent root = fxmlLoader.load(fxmlPath);
+
+            PoupejaApplication.getPrimaryStage().setTitle(titulo);
+            PoupejaApplication.getPrimaryStage().getScene().setRoot(root);
+
+        } catch (IOException e) {
+
+            System.out.println(e);
+            mostrarErro("Erro de Navegação",
+                    "Não foi possível carregar a tela.");
+        }
+    }
+
+    private void mostrarErro(String titulo, String mensagem) {
+
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensagem);
+
+        alert.showAndWait();
     }
 }
